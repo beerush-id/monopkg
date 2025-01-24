b365b4a311d7a1a00f6686ce95daaaa25b834844
Nanang Mahdaen El Agung <mahdaen@gmail.com>
2025-01-25 02:48:25 +0700

    Setting up base path.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/monopkg/',
   title: 'MonoPKG',
   description: 'A simple, yet useful package manager for monorepos',
   themeConfig: {
